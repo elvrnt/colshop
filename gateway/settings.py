aee20ec092fe16dee6a7d38cc88cbcd89515b484
elvrnt <egorovlavrentii@gmail.com>
2026-05-22 23:54:56 +0300

    .

--- gateway/settings.py
+++ gateway/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ek%dk*q+2s(fa#-kyc+jw!&-)5!6&3nub6v%!_xpbmgk@kvz5a'
+SECRET_KEY='django-insecure-ek%dk*q+2s(fa#-kyc+jw!&-)5!6&3nub6v%!_xpbmgk@kvz5a'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
